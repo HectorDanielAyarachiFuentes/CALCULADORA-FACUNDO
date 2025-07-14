d7e45342ec9ecf13a59b86947dccd2c31aa79457
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2025-07-14 13:45:43 -0300

    Listo

--- script.js
+++ script.js
@@ -967,12 +967,3 @@ function raizCuadrada() {
     bajarteclado();
     hideDivExpansionButtons(); // Ocultar después de calcular la raíz
 }
-
-
- if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js')
-                    .then(reg => console.log('Service Worker registrado con éxito:', reg.scope))
-                    .catch(err => console.log('Error al registrar Service Worker:', err));
-            });
-        }
